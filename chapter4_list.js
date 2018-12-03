09b456fd44f4fde75828f49f66de37daee372dda
Sajan Kuttipuravan <skpsajan@gmail.com>
2018-12-03 19:40:49 +0530

    all list funcitons

--- chapter4_list.js
+++ chapter4_list.js
@@ -14,4 +14,32 @@ function arrayToList(a){
 
 console.log(require('util').inspect(arrayToList([99,88,77,66]),true,10))
 
+function listToArray(a){
+   out=[]
+   while(a.rest!==null){
+      out.push(a.value);
+      a=a.rest;
+   };
+   out.push(a.value)
+   return out;
+}
+
+console.log(listToArray(arrayToList([99,88,77,66])))
+
+function prepend(list,elt){
+   return out={value:elt,rest:list}}
+
+console.log(require('util').inspect(prepend(arrayToList([99,88,77,66]),111),true,10))
+
+function nth(list,pos){
+   var a=list.rest
+   for(i=0;i<pos-1;i++){
+      if (a.rest===null){
+         return undefined}
+      a=a.rest;}
+   return a.value}
+
+console.log(nth(prepend(arrayToList([99,88,77,66]),111),4))
+console.log(nth(prepend(arrayToList([99,88,77,66]),111),9))
+
 
